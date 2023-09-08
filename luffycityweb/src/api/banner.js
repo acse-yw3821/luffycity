e91254169d2a73c8570ae886082072555fa447d3
Gatze <18017203725@163.com>
2023-09-08 08:20:45 +0800

    feature:客户端展示轮播广告数据

--- luffycityweb/src/api/banner.js
+++ luffycityweb/src/api/banner.js
@@ -1,22 +1,13 @@
 import http from "../utils/http.js";
 import {reactive, ref} from "vue"
 
-/*
-* 所有于api服务端进行交互的操作代码，
-* 可以单独保存在api目录下，根据数据表单独创建js文件
-*
-* */
-const nav = reactive({
-    header_nav_list: [], // 头部导航列表
-    footer_nav_list: [], // 脚部导航列表
-    get_header_nav() {
-        // 获取头部导航
-        return http.get("/home/nav/header/")
-    },
-    get_footer_nav() {
-        // 获取脚部导航
-        return http.get("/home/nav/footer/")
+
+const banner = reactive({
+    banner_list: [], // 轮播广告列表
+    get_banner_list() {
+        // 获取轮播广告
+        return http.get("/home/banner/")
     },
 })
 
-export default nav;
+export default banner;

18cad2d8f0e9dce09828de50dc9314a3c0cee923
Gatze <18017203725@163.com>
2023-10-26 23:26:33 +0800

    feature: 服务端提供课程分类列表的api接口

--- luffycityapi/luffycityapi/apps/courses/serializers.py
+++ luffycityapi/luffycityapi/apps/courses/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CourseDirection
+from .models import CourseDirection, CourseCategory
 
 
 class CourseDirectionModelSerializer(serializers.ModelSerializer):
@@ -7,4 +7,12 @@ class CourseDirectionModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CourseDirection
+        fields = ["id", "name"]
+
+
+class CourseCategoryModelSerializer(serializers.ModelSerializer):
+    """课程分类的序列化器"""
+
+    class Meta:
+        model = CourseCategory
         fields = ["id", "name"]
--- luffycityapi/luffycityapi/apps/courses/views.py
+++ luffycityapi/luffycityapi/apps/courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
-from .models import CourseDirection
-from .serializers import CourseDirectionModelSerializer
+from .models import CourseDirection, CourseCategory
+from .serializers import CourseDirectionModelSerializer, CourseCategoryModelSerializer
 
 
 # Create your views here.
@@ -11,3 +11,11 @@ class CourseDirectionListAPIView(ListAPIView):
     """学习方向"""
     queryset = CourseDirection.objects.filter(is_show=True, is_deleted=False).order_by("orders", "-id")
     serializer_class = CourseDirectionModelSerializer
+    pagination_class = None
+
+
+class CourseCategoryListAPIView(ListAPIView):
+    """学习分类"""
+    queryset = CourseCategory.objects.filter(is_show=True, is_deleted=False).order_by("orders", "-id")
+    serializer_class = CourseCategoryModelSerializer
+    pagination_class = None
